a339029817580d18fc75ab94a56db1f0b1e926ab
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2022-07-15 11:44:56 +0200

    Implement hamburger functionality

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// function orientationChange() {
+//   if (window.innerWidth > window.innerHeight) {
+//     document.querySelector("body").style.overflow = "visible";
+//     document.querySelector("footer").style.position = "relative";
+//     document.querySelector(".footer-placeholder").style.display = "none";
+//   }
+// }
+// orientationChange();
+
+document.querySelector(".hamburger").addEventListener("click", () => {
+  const hamburgerMenu = document.querySelector(".hamburger-menu");
+  if (hamburgerMenu.style.display == "none") {
+    openHamburgerMenu(hamburgerMenu);
+  } else {
+    closeHamburgerMenu(hamburgerMenu);
+  }
+});
+
+function openHamburgerMenu(menu) {
+  menu.style.display = "block";
+
+  const hamburgerLines = document.querySelectorAll(".hamburger-line");
+  for (line of hamburgerLines) {
+    line.style.display = "none";
+  }
+
+  document.querySelector(".hamburger-close").style.display = "block";
+}
+
+function closeHamburgerMenu(menu) {
+  menu.style.display = "none";
+
+  const hamburgerLines = document.querySelectorAll(".hamburger-line");
+  for (line of hamburgerLines) {
+    line.style.display = "block";
+  }
+
+  document.querySelector(".hamburger-close").style.display = "none";
+}
