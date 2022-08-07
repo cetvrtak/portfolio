22ce24f670266bfb68c0252449ad534364be00e6
cetvrtak <59363714+cetvrtak@users.noreply.github.com>
2022-08-07 16:50:23 +0200

    Implement slider on projects

--- js/script.js
+++ js/script.js
@@ -37,3 +37,78 @@ function closeHamburgerMenu(menu) {
 
   document.querySelector(".hamburger-close").style.display = "none";
 }
+
+//////////////////////////////////////////////////////////
+// Slider component
+//////////////////////////////////////////////////////////
+function slider() {
+  const projects = document.querySelectorAll(".project");
+  const btnLeft = document.querySelector(".chevron-left");
+  const btnRigth = document.querySelector(".chevron-right");
+  let curProject = 0;
+  const maxProject = projects.length;
+  const dotContainer = document.querySelector(".dots");
+
+  function createDots() {
+    projects.forEach((_, i) => {
+      dotContainer.insertAdjacentHTML(
+        "beforeend",
+        `<button class="dots__dot" data-project="${i}"></button>`
+      );
+    });
+  }
+
+  function activateDot(project) {
+    document
+      .querySelectorAll(".dots__dot")
+      .forEach((dot) => dot.classList.remove("dots__dot--active"));
+    document
+      .querySelector(`.dots__dot[data-project="${project}"]`)
+      .classList.add("dots__dot--active");
+  }
+
+  function goToProject(project) {
+    projects.forEach(
+      (s, i) => (s.style.transform = `translateX(${(i - project) * 100}%)`)
+    );
+    activateDot(project);
+  }
+
+  function nextProject() {
+    if (curProject == maxProject - 1) {
+      curProject = 0;
+    } else {
+      curProject++;
+    }
+    goToProject(curProject);
+  }
+  function prevProject() {
+    if (curProject == 0) {
+      curProject = maxProject - 1;
+    } else {
+      curProject--;
+    }
+    goToProject(curProject);
+  }
+  function init() {
+    createDots();
+    goToProject(0);
+  }
+  init();
+
+  btnRigth.addEventListener("click", nextProject);
+  btnLeft.addEventListener("click", prevProject);
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key == "ArrowLeft") prevProject();
+    e.key == "ArrowRight" && nextProject();
+  });
+
+  dotContainer.addEventListener("click", function (e) {
+    if (e.target.classList.contains("dots__dot")) {
+      const { project } = e.target.dataset;
+      goToProject(project);
+    }
+  });
+}
+slider();
